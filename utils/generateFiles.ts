f515fa8f788f65b0b7b799b0cca7b45b34866185
clakr <clarktolosa@gmail.com>
2023-08-08 13:39:02 +0800

    added directory based on project name and css `@layer`

--- utils/generateFiles.ts
+++ utils/generateFiles.ts
@@ -29,96 +29,105 @@ async function generateEnvFile() {
 
   const content = `${projectName}\n${projectLink}\n${projectRepository}`;
 
-  await Bun.write(Bun.file(".env"), content);
+  await Bun.write(Bun.file(`${global.config.title}/.env`), content);
 }
 
 async function generateThenModifyPackageJsonFile() {
   // package.json
   await Bun.write(
-    Bun.file("package.json"),
+    Bun.file(`${global.config.title}/package.json`),
     Bun.file(`${projectRootPath}/templates/html/package.json`)
   );
 
-  const file = Bun.file("package.json");
+  const file = Bun.file(`${global.config.title}/package.json`);
 
   let contents = JSON.parse(await file.text());
   contents = { ...contents, name: global.config.title };
 
-  await Bun.write(Bun.file("package.json"), JSON.stringify(contents, null, 2));
+  await Bun.write(
+    Bun.file(`${global.config.title}/package.json`),
+    JSON.stringify(contents, null, 2)
+  );
 }
 
 async function generateTemplateHTML() {
   // index.html
   await Bun.write(
-    Bun.file("index.html"),
+    Bun.file(`${global.config.title}/index.html`),
     Bun.file(`${projectRootPath}/templates/html/index.html`)
   );
 
   // postcss.config.js
   await Bun.write(
-    Bun.file("postcss.config.js"),
+    Bun.file(`${global.config.title}/postcss.config.js`),
     Bun.file(`${projectRootPath}/templates/html/postcss.config.js`)
   );
 
   // tsconfig.json
   await Bun.write(
-    Bun.file("tsconfig.json"),
+    Bun.file(`${global.config.title}/tsconfig.json`),
     Bun.file(`${projectRootPath}/templates/html/tsconfig.json`)
   );
 
   generateThenModifyPackageJsonFile();
 
-  removeThenMakeDirectory("public");
+  removeThenMakeDirectory(`${global.config.title}/public`);
 
   // public/vite.svg
   await Bun.write(
-    Bun.file("public/vite.svg"),
+    Bun.file(`${global.config.title}/public/vite.svg`),
     Bun.file(`${projectRootPath}/templates/html/public/vite.svg`)
   );
 
-  removeThenMakeDirectory("src");
+  removeThenMakeDirectory(`${global.config.title}/src`);
 
   // src/_vite-env.d.ts
   await Bun.write(
-    Bun.file("src/_vite-env.d.ts"),
+    Bun.file(`${global.config.title}/src/_vite-env.d.ts`),
     Bun.file(`${projectRootPath}/templates/html/src/_vite-env.d.ts`)
   );
 
   // src/_preflight.css
   await Bun.write(
-    Bun.file("src/_preflight.css"),
+    Bun.file(`${global.config.title}/src/_preflight.css`),
     Bun.file(`${projectRootPath}/templates/html/src/_preflight.css`)
   );
 
   // src/style.css
   await Bun.write(
-    Bun.file("src/style.css"),
+    Bun.file(`${global.config.title}/src/style.css`),
     Bun.file(`${projectRootPath}/templates/html/src/style.css`)
   );
 
   // src/_authorModal.css
   await Bun.write(
-    Bun.file("src/_authorModal.css"),
+    Bun.file(`${global.config.title}/src/_authorModal.css`),
     Bun.file(`${projectRootPath}/templates/html/src/_authorModal.css`)
   );
 
   // src/_authorModal.ts
   await Bun.write(
-    Bun.file("src/_authorModal.ts"),
+    Bun.file(`${global.config.title}/src/_authorModal.ts`),
     Bun.file(`${projectRootPath}/templates/html/src/_authorModal.ts`)
   );
 
   // src/main.ts
   await Bun.write(
-    Bun.file("src/main.ts"),
+    Bun.file(`${global.config.title}/src/main.ts`),
     Bun.file(`${projectRootPath}/templates/html/src/main.ts`)
   );
 }
 
 export default async function () {
+  mkdir(global.config.title, (error) => {
+    if (!error) return;
+
+    throw error;
+  });
+
   // .gitignore
   await Bun.write(
-    Bun.file(".gitignore"),
+    Bun.file(`${global.config.title}/.gitignore`),
     Bun.file(`${projectRootPath}/.gitignore`)
   );
 
